32b9a44743b1ab599033dd23c2a4b89e54964fcc
Migue Quiballo <miguelquiballo20@gmail.com>
2024-09-10 11:08:50 +0800

    main

--- Main.java
+++ Main.java
@@ -1,5 +1,36 @@
-public class Division{
-    public int add(int a, int b){
-        return a / b;
+import.java.uti;.Scanner;
+
+public class Main{
+    public static void main(String[]args){
+        Add adder = new Add();
+        Subtract subtractor = new Subtract();
+        Product multiplier = new Product();
+        Division divider= new Division();
+        Modulus modulusOperator = new Modulus();
+
+        Scanner scanner = new scanner(System.in);
+
+        System.out.print("Enter First Number: ");
+        int num1 =scanner.nextInt();
+
+        System.out.print("Enter Second Number: ");
+        int num2 =scanner.nextInt();
+
+        int sum =adder.add(num1,num2);
+        int difference =subtractor.subtract(num1,num2);
+        int product =multiplier.product(num1,num2);
+        int quotient =divider.divide(num1,num2);
+        int modulus =modulusOperator.modulus(num1,num2);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Modulus: " + modulus);
+
+        scanner.close();
+    
+
+        
     }
 }
